02c35c2976257395da41f316cf051860ef6f0809
atulanand26 <atuanand2698@gmail.com>
2025-02-12 10:50:29 +0530

    push

---
+++ partials/footer.php
@@ -0,0 +1,16 @@
+<!-- Footer Section starts -->
+
+<div class="footer">
+	
+	<img src="image/atul-admin-profile-picture.jpg.png" alt="image">
+	<h2 class="footermarg">ATUL ANAND</h2>
+	<p>Copyright © 2022 Atul Anand</p>
+</div>
+<!-- Footer Section ends -->
+
+
+<script src="index1.js"></script>
+</body>
+
+</html>
+
